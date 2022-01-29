f483cbaab25312c5d27c7facbeda5c2c0c27b132
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2022-01-29 22:43:51 +0300

    code add 1

--- skewdimdetect.R
+++ skewdimdetect.R
@@ -29,3 +29,32 @@ for (q in 1:p){
   sampleindices_q = t(apply(combn(p, q), 2, function(x) (1:p %in% x)))
   allsampleindices = rbind(allsampleindices, sampleindices_q)
 }
+
+q_vector = rowSums(allsampleindices)
+
+if (skew_or_kurt_or_both %in% c(1, 3)){
+  if (q_true_skew >= 0 && q_true_skew <= p){
+    sampleindices_true = c(rep(TRUE, q_true_skew), rep(FALSE, (p - q_true_skew)))
+  }else{
+    stop('ERROR in q_true_skew!!')
+  }
+}else if (skew_or_kurt_or_both == 2){
+  if (q_true_kurt >= 0 && q_true_kurt <= p){
+    sampleindices_true = c(rep(TRUE, q_true_kurt), rep(FALSE, (p - q_true_kurt)))
+  }else{
+    stop('ERROR in q_true_kurt!!')
+  }
+}else{
+  stop('ERROR in skew_or_kurt_or_both!!')
+}
+
+index_true = 0
+count_index = 0
+for (i in 1:allsamplenum){
+  if (all(sampleindices_true == allsampleindices[i,])){
+    index_true = i
+    count_index = count_index + 1
+  }
+}
+if (count_index > 1)
+  stop('ERROR!!!!')
