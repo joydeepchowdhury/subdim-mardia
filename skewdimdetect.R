d4b4ef2fa52485797e4bbf1315b0cfe3b7f617ee
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2022-01-29 22:42:57 +0300

    skewness dimension detection script add

---
+++ skewdimdetect.R
@@ -0,0 +1,31 @@
+# p = 5
+# 
+# q_true_skew = 0
+# q_true_kurt = 2
+# t_df = 10
+# 
+# skew_or_kurt_or_both = 1
+# 
+# n = 200
+# 
+# alpha_value = 5
+
+Omega_1 = matrix(0.5, nrow = q_true_skew, ncol = q_true_skew) + diag(0.5, nrow = q_true_skew)
+Omega_2 = matrix(0.5, nrow = (p - q_true_skew), ncol = (p - q_true_skew)) + diag(0.5, nrow = (p - q_true_skew))
+
+alpha_vector = rep(alpha_value, q_true_skew)
+
+####
+signscale_vector = c(1, -0.5, 1, -0.5, 1, -0.5, 1, -0.5, 1, -0.5)
+alpha_vector = alpha_vector * signscale_vector[1:length(alpha_vector)]
+####
+
+Omega_3 = matrix(0.5, nrow = q_true_kurt, ncol = q_true_kurt) + diag(0.5, nrow = q_true_kurt)
+Omega_4 = matrix(0.5, nrow = (p - q_true_kurt), ncol = (p - q_true_kurt)) + diag(0.5, nrow = (p - q_true_kurt))
+
+allsamplenum = 2^p - 1
+allsampleindices = matrix(nrow = 0, ncol = p)
+for (q in 1:p){
+  sampleindices_q = t(apply(combn(p, q), 2, function(x) (1:p %in% x)))
+  allsampleindices = rbind(allsampleindices, sampleindices_q)
+}
