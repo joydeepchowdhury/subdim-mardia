71f8a654a39f8c112e91c3f07e40933d0af3af22
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2022-01-30 17:12:42 +0300

    run file complete

--- skewdimdetect_run.R
+++ skewdimdetect_run.R
@@ -20,3 +20,29 @@
 #   source("~/skewdimdetect.R")
 # }
 
+remove(list = ls())
+
+p = 5
+
+q_true_kurt = 2
+t_df = 10
+
+skew_or_kurt_or_both = 1
+
+n = 500
+
+par(mfrow = c(3, 3))
+for (q_true_skew in 1:3){
+  for (alpha_value in c(1, 3, 5)){
+    source("~/skewdimdetect1.R")
+  }
+}
+par(mfrow = c(3, 3))
+for (q_true_skew in 4:p){
+  for (alpha_value in c(1, 3, 5)){
+    source("~/skewdimdetect1.R")
+  }
+}
+q_true_skew = 0
+source("~/skewdimdetect1.R")
+
