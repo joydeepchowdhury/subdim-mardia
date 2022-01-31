2b0c350973db7929b49d00f6594507bc81e03cdc
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2022-01-31 18:38:28 +0300

    new script add

---
+++ kurtdimdetect.R
@@ -0,0 +1,16 @@
+# p = 5
+# 
+# q_true_skew = 2
+# q_true_kurt = 2
+# t_df = 10
+# 
+# skew_or_kurt_or_both = 2
+# 
+# n = 200
+# 
+# alpha_value = 5
+# 
+# Omega_1 = matrix(0.5, nrow = q_true_skew, ncol = q_true_skew) + diag(0.5, nrow = q_true_skew)
+# Omega_2 = matrix(0.5, nrow = (p - q_true_skew), ncol = (p - q_true_skew)) + diag(0.5, nrow = (p - q_true_skew))
+# 
+# alpha_vector = rep(alpha_value, q_true_skew)
