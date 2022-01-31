1f9925196e1c0f414961e984b4ae8ceaa6ae567e
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2022-01-31 18:38:44 +0300

    code add 1

--- kurtdimdetect.R
+++ kurtdimdetect.R
@@ -14,3 +14,15 @@
 # Omega_2 = matrix(0.5, nrow = (p - q_true_skew), ncol = (p - q_true_skew)) + diag(0.5, nrow = (p - q_true_skew))
 # 
 # alpha_vector = rep(alpha_value, q_true_skew)
+
+Omega_3 = matrix(0.5, nrow = q_true_kurt, ncol = q_true_kurt) + diag(0.5, nrow = q_true_kurt)
+Omega_4 = matrix(0.5, nrow = (p - q_true_kurt), ncol = (p - q_true_kurt)) + diag(0.5, nrow = (p - q_true_kurt))
+
+allsamplenum = 2^p - 1
+allsampleindices = matrix(nrow = 0, ncol = p)
+for (q in 1:p){
+  sampleindices_q = t(apply(combn(p, q), 2, function(x) (1:p %in% x)))
+  allsampleindices = rbind(allsampleindices, sampleindices_q)
+}
+
+q_vector = rowSums(allsampleindices)
