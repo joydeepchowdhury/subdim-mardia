56b1d3797b21bdd05bb839d5a358e3c5bef3b4b5
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2022-02-03 23:35:05 +0300

    code add 5

--- kurtdimdetect.R
+++ kurtdimdetect.R
@@ -114,5 +114,45 @@ parallel_outputs = foreach(index_replicate = 1:num_repl, .combine = rbind, .pack
       }else{
         stop('ERROR in q_true_kurt!!')
       }
+    }else if (skew_or_kurt_or_both == 3){
+      if (q_true_skew == 0){
+        symmetric_components = MASS::mvrnorm(n = n, mu = rep(0, p - q_true_skew), Sigma = Omega_2)
+        Data_temp = symmetric_components
+      }else if (q_true_skew == 1){
+        skewed_components = sn::rsn(n = n, xi = rep(0,length(alpha_vector)), omega = Omega_1, alpha = alpha_vector)
+        symmetric_components = MASS::mvrnorm(n = n, mu = rep(0, p - q_true_skew), Sigma = Omega_2)
+        Data_temp = cbind(skewed_components, symmetric_components)
+      }else if (q_true_skew > 1 && q_true_skew < p){
+        skewed_components = sn::rmsn(n = n, xi = rep(0,length(alpha_vector)), Omega = Omega_1, alpha = alpha_vector)
+        symmetric_components = MASS::mvrnorm(n = n, mu = rep(0, p - q_true_skew), Sigma = Omega_2)
+        Data_temp = cbind(skewed_components, symmetric_components)
+      }else if (q_true_skew == p){
+        skewed_components = sn::rmsn(n = n, xi = rep(0,length(alpha_vector)), Omega = Omega_1, alpha = alpha_vector)
+        Data_temp = skewed_components
+      }else{
+        stop('ERROR in q_true_skew!!')
+      }
+      
+      if (q_true_kurt == 0){
+        normal_components = Data_temp
+        Data = normal_components
+      }else if (q_true_kurt == 1){
+        heavytail_components = Data_temp[, 1] / sqrt(rchisq(n, t_df) / t_df)
+        normal_components = Data_temp[, (q_true_kurt + 1):p]
+        Data = cbind(heavytail_components, normal_components)
+      }else if (q_true_kurt > 1 && q_true_kurt < p){
+        heavytail_components = Data_temp[, 1:q_true_kurt] /
+          matrix(sqrt(rchisq(n, t_df) / t_df), nrow = n, ncol = q_true_kurt, byrow = FALSE)
+        normal_components = Data_temp[, (q_true_kurt + 1):p]
+        Data = cbind(heavytail_components, normal_components)
+      }else if (q_true_kurt == p){
+        heavytail_components = Data_temp[, 1:q_true_kurt] /
+          matrix(sqrt(rchisq(n, t_df) / t_df), nrow = n, ncol = q_true_kurt, byrow = FALSE)
+        Data = heavytail_components
+      }else{
+        stop('ERROR in q_true_kurt!!')
+      }
+    }else{
+      stop('ERROR in skew_or_kurt_or_both!!')
     }
   }
