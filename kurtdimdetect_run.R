4ad29ff1a3853d495092c9ab74f9d345bedfb088
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2022-02-04 22:53:51 +0300

    code add 1

--- kurtdimdetect_run.R
+++ kurtdimdetect_run.R
@@ -8,3 +8,13 @@
 # 
 # n = 200
 # 
+# par(mfrow = c(3, 3))
+# for (q_true_kurt in 0:2){
+#   source("~/kurtdimdetect.R")
+# }
+# par(mfrow = c(3, 3))
+# for (q_true_kurt in 3:p){
+#   source("~/kurtdimdetect.R")
+# }
+
+remove(list = ls())
